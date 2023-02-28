2c8fe3f96f0e8a817481f58f05c9ee4270972aa2
Eric <eric.degomar@gmail.com>
2023-02-28 11:31:03 -0300

    UI iniciada

--- app/src/main/java/com/example/sevenminutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/sevenminutesworkout/MainActivity.kt
@@ -11,5 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    binding.flStart.setOnClickListener {  }
     }
+
 }
