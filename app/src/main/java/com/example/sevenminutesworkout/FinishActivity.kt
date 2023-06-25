755ddb98c2a6f9bf587c6d526e71e96d7b1ac987
Eric <eric.degomar@gmail.com>
2023-06-25 19:19:58 -0300

    adicionado dependencia do lifecyclescope, finalizado o metodo que faz o insert na lista

--- app/src/main/java/com/example/sevenminutesworkout/FinishActivity.kt
+++ app/src/main/java/com/example/sevenminutesworkout/FinishActivity.kt
@@ -1,10 +1,17 @@
 package com.example.sevenminutesworkout
 
+import android.app.Application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.lifecycleScope
 import com.example.sevenminutesworkout.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
     lateinit var binding : ActivityFinishBinding
@@ -24,5 +31,23 @@ class FinishActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        val dao = (application as WorkoutApp).db.historyDao()
+        addDatetoDatabase(dao)
+    }
+
+    fun addDatetoDatabase(historyDAO: HistoryDAO){
+
+        val c = Calendar.getInstance()
+        val dateTime = c.time
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        Log.e("data" , "" + dateTime)
+
+        val date = sdf.format(dateTime)
+
+        lifecycleScope.launch{
+            historyDAO.insert(HistoryEntity(""))
+        }
+
     }
 }
